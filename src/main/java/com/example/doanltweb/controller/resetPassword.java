76f2b8e3549174e3213de0c2c352f28d39db392f
HuynhNghiax <146554474+HuynhNghiax@users.noreply.github.com>
2025-03-09 22:08:27 +0700

    resetPass

---
+++ src/main/java/com/example/doanltweb/controller/resetPassword.java
@@ -0,0 +1,18 @@
+package com.example.doanltweb.controller;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "resetPassword", value = "/resetPassword")
+public class resetPassword extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+}
