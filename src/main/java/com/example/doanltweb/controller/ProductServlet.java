00505c438c1eebf4ecb7a5afade8702acce27aac
HuynhNghiax <146554474+HuynhNghiax@users.noreply.github.com>
2025-05-30 02:23:34 +0700

    chỉnh sửa format và sửa checkout và chỉnh sửa css cho productdetail

--- src/main/java/com/example/doanltweb/controller/ProductServlet.java
+++ src/main/java/com/example/doanltweb/controller/ProductServlet.java
@@ -2,7 +2,10 @@
 package com.example.doanltweb.controller;
 
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
+
 import com.example.doanltweb.dao.model.Product;
 import com.example.doanltweb.dao.model.Supplier;
 import com.example.doanltweb.service.ProductService;
@@ -34,9 +37,8 @@ public class ProductServlet extends HttpServlet {
             out.println("</a>");
             out.println("<div class='card-body themaybom' style='height: 200px'>");
             out.println("<h6 class='card-title'>" + p.getNameProduct() + "</h6>");
-            out.println("<p class='old-price'>Giá cũ: 720.000đ</p>");
-            out.println("<p class='new-price'>Giá mới: 650.000đ</p>");
-            out.println("<a href='chitietsanpham?id=${p.id}' class='btn btn-primary btn-sm'>Xem ngay</a>");
+            out.println("<p class='new-price'>Giá bán: " + NumberFormat.getInstance(new Locale("vi", "VN")).format((long)p.getPriceProduct()) + " vnđ</p>");
+            out.println("<a href='chitietsanpham?id=" + p.getId() + "' class='btn btn-primary btn-sm'>Xem ngay</a>");
             out.println("<button class='btn btn-danger btn-sm ms-4' onclick='addToCart(" + p.getId() + ")'>Mua ngay</button>");
             out.println("</div></div></div>");
         }
